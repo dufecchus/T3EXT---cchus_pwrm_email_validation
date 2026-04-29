97b33e334c58a483912416058348f97a4e810ede
Félix Dumas-Lavoie <felix.dumas-lavoie.ciussse-chus@ssss.gouv.qc.ca>
2026-04-29 14:55:16 -0400

    Ca avance.

--- Resources/Public/JavaScript/cchus_pwrm_email_validation.js
+++ Resources/Public/JavaScript/cchus_pwrm_email_validation.js
@@ -24,6 +24,8 @@ function ready(fn) {
 
     var validationElements = []
     document.querySelectorAll('*[id]').forEach((element,idx) => {
+
+        console.log(element)
         if (element.id.includes("cchus_pwrm_email_validation")){
             var object= {
                 id : element.id.replace("___cchus_pwrm_email_validation",""),
@@ -42,18 +44,23 @@ function ready(fn) {
 
 function verifyFields(validationElements){
     validationElements.forEach((elem)=>{
-        var contentA = document.getElementById(`${elem.id}`).value
-        var contentB = document.getElementById(`${elem.verificationField}`).value  
+        var contentA = document.getElementById(elem.id).value
+        var contentB = document.getElementById(elem.verificationField).value  
         var button = document.querySelector('input[type="submit"]'); 
         var language = navigator.language.split("-")[0]
+
+        var divError = document.createElement('div')
+        divError.setAttribute("id", "pwrm_email_validation_errorMessageDiv")
+        button.parentNode.appendChild(divError)
         
-        document.getElementById(`${elem.id}`).addEventListener("input", (event) => {
+        document.getElementById(elem.id).addEventListener("input", (event) => {
             contentA = document.getElementById(`${elem.id}`).value
             fieldTest()
         })
 
-        document.getElementById(`${elem.verificationField}`).addEventListener("input", (event) => {
-            contentB = document.getElementById(`${elem.verificationField}`).value  
+
+        document.getElementById(elem.verificationField).addEventListener("input", (event) => {
+            contentB = document.getElementById(elem.verificationField).value  
             fieldTest()
 
         })
@@ -67,6 +74,8 @@ function verifyFields(validationElements){
             }
             if (contentA === contentB && contentA !== ""){
                 button.disabled = false;
+                document.getElementById("pwrm_email_validation_errorMessageDiv").innerHTML = ""
+
                 return true;
             }
         }
@@ -79,11 +88,14 @@ function verifyFields(validationElements){
 
         function writeErrorMessage(){
 
-            var elem = document.createElement('div')
-            elem.setAttribute("id", "errorMessageDiv")
-            button.parentNode.appendChild(elem)
+            document.getElementById("pwrm_email_validation_errorMessageDiv").style = "color: red;"
+
             if (language === "fr"){
-            }        
+                document.getElementById("pwrm_email_validation_errorMessageDiv").innerHTML = "Des champs ne correspondent pas. "
+            }
+            else if (language === "en"){
+                document.getElementById("pwrm_email_validation_errorMessageDiv").innerHTML = "Fields does not match."
+            }      
         }
 
 
